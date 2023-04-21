e26ab9f7105a8612ae64208d7608cd3d55ba6bd9
here_io <onedupark@outlook.com>
2023-04-21 12:30:47 +0900

    feat: IList 인터페이스 확장 시도
    결과: 실패.

--- day20_Task/Sort_.cs
+++ day20_Task/Sort_.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,22 +22,102 @@ namespace day20_Task
         * 지정자를 람다식이 아닌 인터페이스 정의 당시에 설정할수 있다는것이 장점이지않을가 생각이든다. 
         * 오늘 배운 반복기를 통해서 배운 객체에 구현된 인터페이스 값을 다른 것으로도 시연해보고 싶었다. 
         */ 
-    public class Sort<T> : IComparable, IEnumerable<Sort>
+    public class Sorting: IComparable//IEnumerable
     {
-        protected IList<T> list; 
-        public IList<T> List { get { return list; } set { list = value; } }
-        
-        public int CompareTo(object? obj)
+        public IList list; 
+        public int index;
+        public int count;
+
+        public Sorting(IList list)
         {
-            if (obj == null) return 1;
-            Sort<T> newSort = obj as Sort<T>;
-            Enumerator<Sort> enumerator = newSort.GetEnumerator();
-            if (newSort != null)
-            {
-                return this.List.
-            }
+            this.list = list;
+            this.count = list.Count; 
+        }
+        public bool IsFixedSize => true;
+
+        public bool IsReadOnly => true;
+
+        public int Count => count;
+
+        public bool IsSynchronized => true;
+
+        public object SyncRoot => true;
+
+        public object? this[int index] { get => list[index]; set { this[index] = value; } }
+
+
+        public void Sort() {  }
+
+        public int Add(object? value)
+        {
             throw new NotImplementedException();
         }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+
 
         // TODO:  try to use IComparer at home 
         //protected int val1; 
@@ -71,55 +152,49 @@ namespace day20_Task
         //            }
         //}
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return new Enumerator(this.list); 
-        }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return new Enumerator(this.list);
-        }
+        //망
+        //public struct Enumerator : IEnumerator
+        //{
+        //    private int index;
+        //    private Sort sort; 
+        //    private IList list;
+        //    private object? current; 
 
-        public struct Enumerator : IEnumerator<T>
-        {
-            private int index;
-            private IList<T> list;
-            private T current; 
+        //    public Enumerator(Sort sort)
+        //    {
+        //        this.sort = sort;
+        //        this.list = sort.List;
+        //        this.index = 0;
+        //        this.current = default(object);
+        //    }
+        //    public object Current => current;
 
-            public Enumerator(IList<T> list)
-            {
-                this.list = list;
-                this.index = 0;
-                this.current = default(T);
-            }
-            public T Current => current;
+        //    object IEnumerator.Current => (object)current;
 
-            object IEnumerator.Current => (object)current;
+        //    public void Dispose()
+        //    {
 
-            public void Dispose()
-            {
+        //    }
 
-            }
+        //    public bool MoveNext()
+        //    {
+        //        if (index < list.Count)
+        //        {
+        //            current = list[index++];
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            current = default(object);
+        //            return false;
+        //        }
+        //    }
 
-            public bool MoveNext()
-            {
-                if (index < list.Count)
-                {
-                    current = list[index++];
-                    return true;
-                }
-                else
-                {
-                    current = default(T);
-                    return false;
-                }
-            }
-
-            public void Reset()
-            {
-                index = 0;
-            }
-        }
+        //    public void Reset()
+        //    {
+        //        index = 0;
+        //    }
+        //} // 망 //망
     }
 }
