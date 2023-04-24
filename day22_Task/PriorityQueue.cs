5027879463f0f79fa5d4e057ede6540f171a0e03
ChoiByoungChan <qk123987@gmail.com>
2023-04-24 21:45:00 +0900

    으어어 왜 안되지

--- day22_Task/PriorityQueue.cs
+++ day22_Task/PriorityQueue.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,18 +16,23 @@ namespace Task_DataStructure
     /// <typeparam name="TElement"></typeparam>
     public class PriorityQueue<TElement>
     {
+
         /// <summary>
         /// 들어오는 값에 대해서 실존하는 값과, 우선순위를 표기하기 위한 최소한의 단위를 구조체로 생성하여준다. 
         /// </summary>
-        public struct Node
+        private struct Node
         {
-            public TElement element { get; set; }
-            public int priority { get; set; } // IComparable 을 통해서도 값을 지정하여줄수 있으나, 그렇게 할까..? nvm not yet 
+            public TElement element;
+            public int priority; 
+            // IComparable 을 통해서도 값을 지정하여줄수 있으나, 그렇게 할까..? nvm not yet 
             // int의 default Comparer 를 사용하기 때문에, it's Ascending 으로 Sort 되기 때문에 lower Number = higher Priority 으로 설정하고 진행하였다. 
         }
 
-        List<Node> nodes; 
-
+        private List<Node> nodes; 
+        public int Count
+        {
+            get { return nodes.Count; }
+        }
         /// <summary>
         /// 최초 선언시, 할건 딱히 없다, 힙구조자료는 List를 돚거/아답트 하여 리스트를 이용하며 새로운 기능을 구현한다. 
         /// </summary>
@@ -41,12 +47,12 @@ namespace Task_DataStructure
         /// <param name="priority"></param>
         public void Enqueue(TElement element, int priority)
         {
-            Node newNode = new Node // 새로운 값 어서오고 
-            {
-                element = element,
-                priority = priority
-            };
-            nodes.Add(newNode);
+            // 새로운 값 생성시, 해당 값은 자연스럽게 리스트의 마지막에 추가가 된다.
+            Node newNode = new Node();
+            newNode.element = element;
+            newNode.priority = priority;
+
+            //nodes[Count-1] = newNode;
             int newIndex = nodes.Count - 1;
 
             while (newIndex > 0)// 만약 새로운 인덱스값이 0이라면 최초값이기에, 힙정렬을 진행할 이유가 전혀 없을것이다. 
@@ -57,40 +63,72 @@ namespace Task_DataStructure
                 if (newIndex < nodes.Count)// 멀쩡한 인덱스 값이라면 (List 에서), List의 카운트 보다 항상 작을것이다. // 매우 간단하게 정상적인 index값인지 확인하는 방법이다.  
                 {
                     if (nodes[newIndex].priority < nodes[parentIndex].priority)
-                        Swap(ref newNode, ref parent);
+                    {
+                        nodes[parentIndex] = nodes[newIndex];
+                        nodes[newIndex] = newNode;
+                        parentIndex = newIndex;
+                    }
                     else // 더이상 신입이 상위 노드보다 위에 있지 않다면 힙상태 도달 완료 (where Top.Priority > Mid.Priority > Low.Priority) 
                         break;
                 }   
             }
         }
 
-        public void Dequeue()
+        public TElement Dequeue()
         {
             Node ancestor = nodes[0]; // 우선 밖으로 추출할 조상님 저장 (구조체로) 
             Node lastNode = nodes[nodes.Count - 1];
             nodes[0] = lastNode; //힙정렬을 위하여 마지막 값에 저장된 값을 맨앞으로 부르고, 위에서부터 아래로 힙정렬 시전 
             int root = 0; 
-            while (nodes.Count > 0)
+            while (root < nodes.Count)
             {
                 int L_Index = LChildIndex(root);
-                int R_Index = RChildIndex(root); 
+                int R_Index = RChildIndex(root);
                 // Case 1. There's 2 Child Ndoes 
-
-                // Case 2. There's 1 Child Node (Left, given we are working on a List, (if there's only 1, in a binary tree, RemoveAt(마지막)을 통해서 값을 삭제하였기에, 삭제되는 순서는 오른쪽, 왼쪽순으로 간다는것을 전제로 해도 된다. 
+                if (R_Index < nodes.Count)
+                {
+                    int target = nodes[L_Index].priority < nodes[R_Index].priority ? L_Index : R_Index;
+                    if (nodes[target].priority < nodes[root].priority)
+                    {
+                        nodes[target] = nodes[root];
+                        nodes[root] = lastNode;
+                        target = root;
+                        root = target;
+                    }
+                    else
+                        break;
+                }
+                // 이것의 목적은 힙정렬이다. 그러므로 힙상태를 위해 상위클래스는 무조건 더 높은 우선순위를 가진 노드가 올라가야만 한다 
+                // Case 2. There's 1 Child Node (Left, given we are working on a List, (if there's only 1, in a binary tree, RemoveAt(마지막)을 통해서 값을 삭제하였기에, 삭제되는 순서는 오른쪽, 왼쪽순으로 간다는것을 전제로 해도 된다.
+                else if (L_Index < nodes.Count)
+                {
+                    if (nodes[L_Index].priority < nodes[root].priority)
+                    {
+                        nodes[L_Index] = nodes[root];
+                        nodes[root] = lastNode;
+                        L_Index = root;
+                        root = L_Index;
+                    }
+                    else
+                        break;
+                }
+                else
+                    break; 
                 // Case 3. 하위노드가 없다. 
             }
+            return ancestor.element; 
         }
 
         public TElement Peek()
         {
             return this.nodes[0].element; 
         }
-        public void Swap(ref Node first, ref Node second)
-        {
-            Node temp = first;
-            first = second;
-            second = temp;
-        }
+        //public void Swap(int first, int second, Node n_first)
+        //{
+        //    nodes[second] = nodes[first];
+        //    nodes[first] = n_first;
+        //    second = first;
+        //}
         public int GetParentNode(int childIndex)
         {
             return (childIndex - 1) / 2;
--- day22_Task/Program.cs
+++ day22_Task/Program.cs
@@ -6,13 +6,51 @@
      * ------------------------------------------------------------------
      * 응급실 만들기 patient = (string name, int hp(?))
      * 중간값 구하기 
-     */ 
-
-    internal class Program
+     */
+    public class Program
     {
+        // 힙은,  * 많은 자료 중 우선순위가 가장 높은 요소를 빠르게 가져오기 위해 사용하게 된다. 
+        // 부모 (노드)가 항상 자식(노드)보다 우선순위가 높은 속성을 만족하는 트리기반의 자료구조
+        // 사용 사례 
+        // Big O    접근      탐색      
+        // 
+        public static void PriorityQueue()
+        {
+            Task_DataStructure.PriorityQueue<string> pq = new Task_DataStructure.PriorityQueue<string>();
+
+            pq.Enqueue("감자", 3); // 이때 int 는 우선순위대상으로 가늠이 되는값이면 된다. (?)
+            pq.Enqueue("양파", 5);
+            pq.Enqueue("당근", 1);
+            pq.Enqueue("토마토", 2);
+            pq.Enqueue("마늘", 4);
+            // 만약 숫자로 지정해준 값이 같을때, 나중에 들어온 element가 먼저 나오게 된다.
+            while (pq.Count > 0)
+            {
+                Console.WriteLine(pq.Dequeue());
+            }
+            // Priority_Queue 는 Ascending, Descending 으로 제정립도 가능한데, default값은 Ascending 으로 제정되어있다 (해당 IComparer 메서드는) 
+
+            //Descending 
+            PriorityQueue<string, int> desc_pq = new PriorityQueue<string, int>(Comparer<int>.Create((a, b) => b - a));
+            // where in Comparer/ and IComparable, in Lambda Expression, default = (a,b) => a -b .
+            // 만약 a가 더 크면 >0 을 반환하게 되며 작다면 0<을 반환한다
+            // 때문에, 알수있는 사실은 
+            // if value >0, a 가 b 보다 작음으로, a가 b보다 (왼쪽기준) 먼저 와야하며,
+            // value <0, b가 a 보다 작음으로, a는 b보다 오른쪽에 배치되야함을 시사한다
+            // , 0 값이 같음, 배열에 변화가 없어도 된다. 
+
+            // 따라서 b-a 라면, asc 와 반대 기준으로 정렬을 하게 된다. 
+            // 이는 comparer 의 (return int 값이 0 > 이라면 우측값을 앞으로 배치하기 때문이다. 
+
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            PriorityQueue();
         }
+
+
     }
 }
