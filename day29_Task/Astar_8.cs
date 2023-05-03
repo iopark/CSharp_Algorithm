62545d2f9d979ed5ba31cf102da0bc18d87caf60
here_io <onedupark@outlook.com>
2023-05-04 01:33:37 +0900

    feat: AStar 4 Direction, 8 Direction 구현 성공 및 메인에서 테스트, 프린트 완료

--- day29_Task/Astar_8.cs
+++ day29_Task/Astar_8.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.XPath;
@@ -12,62 +13,63 @@ namespace day29_Task
     // 다만 불가능의 조건에 도달하게 되었을때는, 대기중인 포인트로 백트래킹하여 계속하여 목표접에 접근을 시도할수 있다. 
     // 따라서 Heuristic 을 찾기 위한 조건은 Grid 기준 Metric function 을 이용하지만, Grid 가 아닌 것에 대해서도 이용하는것이 가능하며, 
     // Direction의 개념에 대해서 선형적으로 책정하는것뿐만아니라, 다양하게 선정이 가능하겠다. 
-    public class Astar
+    public class Astar_8
     {
         const int vert_horizontal = 10;
         const int diagonal = 14;
-        static Point[] Direction =
+        static Point_8[] Direction =
         {
-            new Point (0, 1),       // Top
-            new Point (0, -1),      // Bottom
-            new Point (-1, 0),      // Left
-            new Point (1, 0)       // Right 
-            //new Point( -1, +1 ),    // Left Top[ 
-			//new Point( -1, -1 ),    // Left Bottom 
-			//new Point( +1, +1 ),    // Right Top 
-			//new Point( +1, -1 )     // Right bottom  
+            new Point_8( 0, 1 ),       // Top
+            new Point_8( 0, -1 ),      // Bottom
+            new Point_8( -1, 0 ),      // Left
+            new Point_8( 1, 0 ),       // Right 
+            new Point_8( -1, +1 ),    // Left Top 
+			new Point_8( -1, -1 ),    // Left Bottom 
+			new Point_8( +1, +1 ),    // Right Top 
+			new Point_8( +1, -1 )     // Right bottom  
         };
 
         // where tile = [y, x] flipped x,y for sake of graphic rendering 
-        public static bool ShortestPath(bool[,] Map, Point start, Point end, out List<Point> shortestpath)
+        public static bool ShortestPath_8(bool[,] Map, Point_8 start, Point_8 end, out List<Point_8> shortestpath)
         {
             int sizeY = Map.GetLength(0);
             int sizeX = Map.GetLength(1);
-            shortestpath = new List<Point>();
+            shortestpath = new List<Point_8>();
             bool[,] visited = new bool[sizeY, sizeX];
             PriorityQueue<StarNode, int> contestingNodes = new PriorityQueue<StarNode, int>();
-
+            Stack<StarNode> traceNodes = new Stack<StarNode>();
             // must also have a matrix which saves vertices prior to the 'search'. 
             StarNode[,] nodes = new StarNode[sizeY, sizeX];// 이건 나중에 route 를 전달할때에 용이하게 사용될수 있다. 
 
             // Start with the node of the starting position 
 
             StarNode initial = new StarNode(start, null, 0, Heuristic(start, end));
-            contestingNodes.Enqueue(initial, initial.f); 
-
+            contestingNodes.Enqueue(initial, initial.f);
+            
             while (contestingNodes.Count > 0)
             {
 
                 StarNode contestant = contestingNodes.Dequeue();
                 //Welcome to the testing ground 
+                traceNodes.Push(contestant);
                 visited[contestant.point.y, contestant.point.x] = true;
                 nodes[contestant.point.y,contestant.point.x] = contestant;
                 // if contestant is the final node 
                 if (contestant.point.x == end.x && contestant.point.y == end.y)
                 {
                     // must retrive the route which has been taken to get to the destination 
-                    Point? toInitial = contestant.point;
+                    Point_8? toInitial = contestant.point;
                     // null 이 아닐때 까지 path 에 저장해야만 하는데, 
                     while (toInitial != null)
                     {
                         // 1. path 에 해당 point 저장 
                         // 2. 해당 point 의 parent point 를 반복문을 위해 저장 
-                        Point previous = toInitial.GetValueOrDefault(); 
+                        Point_8 previous = toInitial.GetValueOrDefault(); 
                         shortestpath.Add(previous); 
                         toInitial = nodes[previous.y, previous.x].parent;
                     }
                     shortestpath.Reverse();
-                    Console.WriteLine("종착역에 이르었습니다"); 
+                    Console.WriteLine("팔각역 종착역에 이르었습니다"); 
                     return true; 
                 }
 
@@ -88,9 +90,14 @@ namespace day29_Task
                         continue;
                     else if (visited[new_y, new_x])
                         continue;
+                    // 대각선 값에 대해서 추가적으로 더 확인한다 
+                    else if (DiagonalAlley(Map, contestant.point,i) == false)
+                        continue;
 
-                    Point temp_point = new Point(new_x, new_y);
-                    int g = contestant.g + 10; // for vert/horizontal only 
+
+                    Point_8 temp_point = new Point_8(new_x, new_y);
+                    //int g = contestant.g + 10; // for vert/horizontal only 
+                    int g = contestant.g + (temp_point.x == end.x || temp_point.y == end.y ? vert_horizontal : diagonal);
                     int h = Heuristic(temp_point, end);
                     //이후에 추가하기전 마지막으로 검사해야할것은 만약 해당 정점값이 이미 추가가 된 값이라면,
                     //또는.f 값과 비교해 목표치로 더 유망하다면 스왑, 아니라면 포기해야합니다 
@@ -101,19 +108,73 @@ namespace day29_Task
                     {
                         nodes[new_y,new_x] = temporary;
                         contestingNodes.Enqueue(temporary, temporary.f);
+                        traceNodes.Push(temporary);
                     }
                 }
             }
             // if through the loop, false is not given and loop is broken, there's no valid route to destination in this map. 
-            shortestpath = null;
+            StarNode lastNode = traceNodes.Pop();
+            Point_8? tracetoFirst = lastNode.point;
+            // null 이 아닐때 까지 path 에 저장해야만 하는데, 
+            while (traceNodes.Count > 0)
+            {
+                // 1. path 에 해당 point 저장 
+                // 2. 해당 point 의 parent point 를 반복문을 위해 저장 
+                Point_8 previous = tracetoFirst.GetValueOrDefault();
+                shortestpath.Add(previous);
+                tracetoFirst = traceNodes.Pop().point;
+            }
+            shortestpath.Reverse();
             Console.WriteLine("없어요 여기 종착점");
             return false; 
             
 
 
         }
+        // 만약 대각선 값에 대해서, 예시: 좌상인 경우, 좌 와 상 모두 벽이 있다면, false 
+        // 그게 아니라면 true 
+        // 대각선값에 대해서 취급하는게 아니라면 null을 반환합니다. 
+        private static bool? DiagonalAlley(bool[,] tileMap, Point_8 candidate, int direction)
+        {
+            int left = candidate.x - 1; 
+            int right = candidate.x + 1;
+            int top = candidate.y + 1;
+            int bottom = candidate.y - 1;
+            if (direction >= 4)
+            {
+                switch (direction)
+                {
+                    // leftTop 
+                    case 4:
+                        if (tileMap[candidate.y, left] == false && tileMap[top, candidate.x] == false)
+                            return false;
+                        else
+                            return true;
+                    // leftBottom 
+                    case 5:
+                        if (tileMap[candidate.y, left] == false && tileMap[bottom, candidate.x] == false)
+                            return false;
+                        else
+                            return true;
+                    // rightTop 
+                    case 6:
+                        if (tileMap[candidate.y, right] == false && tileMap[top, candidate.x] == false)
+                            return false;
+                        else
+                            return true;
+                    // rightBottom 
+                    case 7:
+                        if (tileMap[candidate.y, right] == false && tileMap[bottom, candidate.x] == false)
+                            return false;
+                        else
+                            return true;
+                }
 
-        private static int Heuristic(Point start, Point end)
+            }
+            return null;
+        }
+
+        private static int Heuristic(Point_8 start, Point_8 end)
         {
             // Using Euclidean distance for the Heuristic value, in deciding whether contesting node is close to the final destination 
             int x = Math.Abs(start.x - end.x);
@@ -123,13 +184,13 @@ namespace day29_Task
         }
         public class StarNode
         {
-            public Point point;
-            public Point? parent;
+            public Point_8 point;
+            public Point_8? parent;
             public int f; // f = g + h
             public int g; // accumulated value from previous routes 
             public int h; // predicted routes based on certain distance calculation Metric
 
-            public StarNode(Point point, Point? parent, int g, int h)
+            public StarNode(Point_8 point, Point_8? parent, int g, int h)
             {
                 this.point = point;
                 this.parent = parent;
@@ -140,11 +201,11 @@ namespace day29_Task
         }
         
     }
-    public struct Point
+    public struct Point_8
     {
         public int x;
         public int y;
-        public Point(int x, int y)
+        public Point_8(int x, int y)
         {
             this.x = x;
             this.y = y;
--- day29_Task/Program.cs
+++ day29_Task/Program.cs
@@ -16,6 +16,101 @@
                 { false,  true,  true,  true,  true,  true,  true,  true, false },
                 { false, false, false, false, false, false, false, false, false },
             };
+
+            bool[,] map_blocked = new bool[9, 9]
+            {
+                { false, false, false, false, false, false, false, false, false },
+                { false,  true,  true,  true, false, false, false,  true, false },
+                { false,  true, false,  true, false, false, false,  true, false },
+                { false,  true, false,  true,  true,  true,  true,  true, false },
+                { false,  true, false,  true, false, false, false,  true, false },
+                { false,  true, false,  true, false, false, false,  true, false },
+                { false, false, false, false, false, false, false,  true, false },
+                { false,  true,  true,  true,  true,  true,  true,  false, false },
+                { false, false, false, false, false, false, false, false, false },
+            };
+            List<Point> shortestpath; 
+            Astar.ShortestPath(map, new Point(1, 1), new Point(1, 7), out shortestpath);
+
+            List<Point_8> shortestpath_Diag;
+            Astar_8.ShortestPath_8(map, new Point_8(1, 1), new Point_8(1, 7), out shortestpath_Diag); 
+
+            PrintResult(map, shortestpath);
+
+            PrintResult_8(map, shortestpath_Diag, new Point_8(1, 7), new Point_8(1, 1));
+
+            List<Point_8> shortestpath_Diag_Blocked;
+
+            Astar_8.ShortestPath_8(map_blocked, new Point_8(1, 1), new Point_8(1, 7), out shortestpath_Diag_Blocked);
+            PrintResult_8(map_blocked, shortestpath_Diag_Blocked, new Point_8(1, 7), new Point_8(1, 1));
+        }
+
+        static void PrintResult(in bool[,] tileMap, in List<Point> path)
+        {
+            char[,] pathMap = new char[tileMap.GetLength(0), tileMap.GetLength(1)];
+            for (int y = 0; y < pathMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < pathMap.GetLength(1); x++)
+                {
+                    if (tileMap[y, x])
+                        pathMap[y, x] = ' ';
+                    else
+                        pathMap[y, x] = '#';
+                }
+            }
+
+            foreach (Point point in path)
+            {
+                pathMap[point.y, point.x] = '*';
+            }
+
+            Point start = path.First();
+            Point end = path.Last();
+            pathMap[start.y, start.x] = 'S';
+            pathMap[end.y, end.x] = 'E';
+
+            for (int i = 0; i < pathMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < pathMap.GetLength(1); j++)
+                {
+                    Console.Write(pathMap[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintResult_8(in bool[,] tileMap, in List<Point_8> path, Point_8 endPoint, Point_8 startPoint)
+        {
+            char[,] pathMap = new char[tileMap.GetLength(0), tileMap.GetLength(1)];
+            for (int y = 0; y < pathMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < pathMap.GetLength(1); x++)
+                {
+                    if (tileMap[y, x])
+                        pathMap[y, x] = ' ';
+                    else
+                        pathMap[y, x] = '#';
+                }
+            }
+
+            foreach (Point_8 point in path)
+            {
+                pathMap[point.y, point.x] = '*';
+            }
+
+            Point_8 start = startPoint; // ~~
+            Point_8 end = endPoint; // 값을 찾지 못한 상황연출을 위한 임의설정
+            pathMap[start.y, start.x] = 'S';
+            pathMap[end.y, end.x] = 'E';
+
+            for (int i = 0; i < pathMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < pathMap.GetLength(1); j++)
+                {
+                    Console.Write(pathMap[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
